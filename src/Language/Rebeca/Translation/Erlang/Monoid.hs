52c33054d5b4b641a443b2c5e9781f4ea302c7a1
Árni Hermann Reynisson <arnihr@gmail.com>
2011-05-11 10:04:01 +0000

    Working on monoids.

--- src/Language/Rebeca/Translation/Erlang/Monoid.hs
+++ src/Language/Rebeca/Translation/Erlang/Monoid.hs
@@ -8,14 +8,15 @@ import Language.Rebeca.Absrebeca
 import Language.Rebeca.Fold
 
 {-monoidAlgebra :: (Monoid id, Monoid mod, Monoid env, Monoid rc, Monoid kr, Monoid sv, Monoid msi, Monoid ms, Monoid vd, Monoid tvd, Monoid tp, Monoid bt, Monoid tn, Monoid stm, Monoid cs, Monoid aft, Monoid dea, Monoid eli, Monoid el, Monoid exp, Monoid con, Monoid uop, Monoid aop, Monoid mai, Monoid ins) => RebecaAlgebra id mod env rc kr sv msi ms vd tvd tp bt tn stm cs aft dea eli el exp con uop aop mai ins-}
+monoidAlgebra :: (Monoid m, Monoid ms) => RebecaAlgebra m [ms] ms [ms] m [ms] m m m [ms] [ms] m m [ms] m m m [ms] m [ms] m m m [ms] m
 monoidAlgebra = RebecaAlgebra {
-    identF = mempty
+    identF = \s -> mempty
 
-  , modelF = \envs rcs mai -> mempty
+  , modelF = \envs rcs mai -> envs `mappend` rcs `mappend` mai
 
   , envVarF = \_ -> mempty
 
-  , reactiveClassF = \id _ kr sv msi ms -> mempty
+  , reactiveClassF = \id _ kr sv msi ms -> sv
 
   , noKnownRebecsF = mempty
   , knownRebecsF = \_ -> mempty
@@ -39,8 +40,8 @@ monoidAlgebra = RebecaAlgebra {
   , basicTypeTimeF = mempty
   , basicTypeBooleanF = mempty
 
-  , builtInF = mempty
-  , classTypeF = mempty
+  , builtInF = \_ -> mempty
+  , classTypeF = \_ -> mempty
 
   , assF = \id aop exp -> mempty
   , localF = \tvd -> mempty
--- src/Language/Rebeca/Translation/Erlang/Variables.hs
+++ src/Language/Rebeca/Translation/Erlang/Variables.hs
@@ -9,9 +9,10 @@ import Language.Rebeca.Fold
 
 import Language.Rebeca.Translation.Erlang.Monoid
 
-{-stateVarsAlgebra :: (Monoid id, Monoid mod, Monoid env, Monoid rc, Monoid kr, Monoid sv, Monoid msi, Monoid ms, Monoid vd, Monoid tvd, Monoid tp, Monoid bt, Monoid tn, Monoid stm, Monoid cs, Monoid aft, Monoid dea, Monoid eli, Monoid el, Monoid exp, Monoid con, Monoid uop, Monoid aop, Monoid mai, Monoid ins) => RebecaAlgebra id mod env rc kr sv msi ms vd tvd tp bt tn stm cs aft dea eli el exp con uop aop mai ins-}
+{-stateVarsAlgebra :: Monoid m => RebecaAlgebra m m m m m m m m m m m m m m m m m m m m m m m m m-}
 stateVarsAlgebra = monoidAlgebra {
-    stateVarsF = \tvds -> tvds
+    identF = \s -> [s]
+  , stateVarsF = \tvds -> tvds
 }
 
 stateVarNames :: Model -> [String]
